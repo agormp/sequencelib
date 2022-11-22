0b285e50d28cf9f96280dafaf57bd963a1ba1a16
Anders Gorm Pedersen <agpe@dtu.dk>
2022-11-22 10:01:42 +0100

    Add tests

--- tests/test_sequencelib.py
+++ tests/test_sequencelib.py
@@ -1,33 +1,98 @@
 import pytest
 import sequencelib as sq
+import random
 
-###############################################################################
+
+###################################################################################################
+###################################################################################################
+
+# Tests for loose functions
+
+###################################################################################################
+###################################################################################################
 
 class Test_find_seqtype:
     
-    def test_DNA_noambig(self, DNA_string):
+    seqlen = 75
+    
+    def test_DNA_noambig(self):
+        DNA_string = "".join(random.choices(list(sq.Const.DNA), k=self.seqlen))
         assert sq.find_seqtype(DNA_string) == "DNA"
         assert sq.find_seqtype(list(DNA_string)) == "DNA"
         assert sq.find_seqtype(set(DNA_string)) == "DNA"
         
-    def test_DNA_ambig(self, DNA_string_ambig):
+    def test_DNA_ambig(self):
+        DNA_string_ambig = "".join(random.choices(list(sq.Const.DNA_maxambig), k=self.seqlen))
         assert sq.find_seqtype(DNA_string_ambig) == "DNA"
         assert sq.find_seqtype(list(DNA_string_ambig)) == "DNA"
         assert sq.find_seqtype(set(DNA_string_ambig)) == "DNA"
         
-    def test_Protein_noambig(self, Protein_string):
+    def test_Protein_noambig(self):
+        Protein_string = "".join(random.choices(list(sq.Const.Protein), k=self.seqlen))
         assert sq.find_seqtype(Protein_string) == "protein"
         assert sq.find_seqtype(list(Protein_string)) == "protein"
         assert sq.find_seqtype(set(Protein_string)) == "protein"
         
-    def test_Protein_ambig(self, Protein_string_ambig):
+    def test_Protein_ambig(self):
+        Protein_string_ambig = "".join(random.choices(list(sq.Const.Protein_maxambig), k=self.seqlen))
         assert sq.find_seqtype(Protein_string_ambig) == "protein"
         assert sq.find_seqtype(list(Protein_string_ambig)) == "protein"
         assert sq.find_seqtype(set(Protein_string_ambig)) == "protein"
 
-    def test_ASCII(self, ASCII_string):
+    def test_ASCII(self):
+        ASCII_string = "".join(random.choices(list(sq.Const.ASCII), k=self.seqlen))
         assert sq.find_seqtype(ASCII_string) == "ASCII"
         assert sq.find_seqtype(list(ASCII_string)) == "ASCII"
         assert sq.find_seqtype(set(ASCII_string)) == "ASCII"
+        
+    def test_unrecognized_raises(self):
+        ASCII_string = "".join(random.choices(list(sq.Const.ASCII), k=self.seqlen))
+        unknown = ASCII_string + "ØÆÅ=)&%#"
+        with pytest.raises(sq.SeqError):
+            sq.find_seqtype(unknown)
 
-###############################################################################
+###################################################################################################
+
+class Test_seqtype_attributes:
+    
+    def test_DNA(self):
+        assert (sq.seqtype_attributes("DNA") 
+                == (set("ACGTURYMKWSBDHVN"), set("URYMKWSBDHVN")))
+
+    def test_Protein(self):
+        assert (sq.seqtype_attributes("protein") 
+                == (set("ACDEFGHIKLMNPQRSTVWYBZX"), set("BXZ")))
+        
+    def test_ASCII(self):
+        assert (sq.seqtype_attributes("ASCII") 
+                == (set("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,._"), set()))
+    
+    def test_unknown_raises(self):
+        with pytest.raises(sq.SeqError):
+            sq.seqtype_attributes("normannisk")
+
+###################################################################################################
+
+class Test_indices:
+    
+    def test_singlesubstring(self):
+        inputstring = "AAAAAAAAAA AAAAAAAA AAAAAAA Here AAAAAAAA AAAAAA"
+        assert sq.indices(inputstring, "Here") == set([28])
+        
+    def test_triplesubstring(self):
+        inputstring = "AAAAAAAAAA Here AAAAAAAA AAAAAAA Here AAAAAAAA AAAHereAAA"
+        assert sq.indices(inputstring, "Here") == set([11,33,50])
+        
+    def test_overlapping(self):
+        inputstring = "AAAAAAAAAA hehehehe AAAAAAA hehe AAAAA"
+        assert sq.indices(inputstring, "hehe") == set([11,13,15,28])
+    
+
+###################################################################################################
+###################################################################################################
+
+# Tests for class XXX
+
+###################################################################################################
+###################################################################################################
+
